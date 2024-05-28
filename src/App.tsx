cfa40eb686f8b9b94b8071b2af9900a6b9c32a34
Fatih Mahawisesa <fatihmahawisesa@gmail.com>
2024-05-28 15:06:56 +0700

    DSAASD

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ function App() {
   window.addEventListener('message', (event) => {
     // Do not do anything unless the message was from
     // a domain we trust.
-    if (event.origin !== 'http://localhost:5174/') { console.log("gagal"); return; }
+
+    console.log("epent orijin " , event.origin)
+    if (event.origin !== 'http://localhost:5174') { console.log("gagal"); return; }
 
     // Create a local copy of the variable we were passed.
     var test_parameter = event.data;
