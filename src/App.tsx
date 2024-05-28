356057af1b2157aff4470a2db4b9a7b8b7eea8ac
Fatih Mahawisesa <fatihmahawisesa@gmail.com>
2024-05-28 14:34:01 +0700

    kutak membencimu

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,20 @@ import validation from "./default/validation.json";
 function App() {
 
 
-  console.log("window.my_special_setting")
-  let meBe = localStorage.getItem('testing');
-  console.log(meBe);
+  window.addEventListener('message', (event) => {
+    
+    // Do not do anything unless the message was from
+    // a domain we trust.
+    if (event.origin !== 'http://localhost:5174/') {
+      console.log("gagal");
+      return;
+  }
+
+    // Create a local copy of the variable we were passed.
+    var test_parameter = event.data;
+    console.log("test");
+    console.log(test_parameter);
+})
 
   let obj = {
     answers: [],
