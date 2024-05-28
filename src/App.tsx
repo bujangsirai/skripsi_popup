36ec2f3073c7cf9eb7f5d75ef0f15e8a1af5a836
Fatih Mahawisesa <fatihmahawisesa@gmail.com>
2024-05-28 15:05:42 +0700

    A

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ import validation from "./default/validation.json";
 
 function App() {
 
+  window.addEventListener('message', (event) => {
+    // Do not do anything unless the message was from
+    // a domain we trust.
+    if (event.origin !== 'http://localhost:5174/') { console.log("gagal"); return; }
+
+    // Create a local copy of the variable we were passed.
+    var test_parameter = event.data;
+    console.log("test");
+    console.log(test_parameter);
+})
+
 
   let obj = {
     answers: [],
--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,6 @@ import { render } from 'solid-js/web'
 import './index.css'
 import App from './App'
 
-window.addEventListener('message', (event) => {
-    // Do not do anything unless the message was from
-    // a domain we trust.
-//     if (event.origin !== 'http://localhost:5174/') {
-//       console.log("gagal");
-//       return;
-//   }
-
-    // Create a local copy of the variable we were passed.
-    var test_parameter = event.data;
-    console.log("test");
-    console.log(test_parameter);
-})
-
-
 const root = document.getElementById('root')
 
 render(() => <App />, root!)
